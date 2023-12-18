6efa6db571a09c943fd40021196acc5491ef06e7
Dan Federman <dfed@me.com>
2023-12-17 20:34:56 -0800

    Add ExampleProjectIntegration/Package.swift to make developing SafeDI's package simpler

---
+++ Examples/ExampleProjectIntegration/Package.swift
@@ -0,0 +1,8 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+// This package is here because otherwise this project's code will appear when opening SafeDI's Package.swift file.
+// This package file serves no other purpose.
+let package = Package(name: "IgnoreMe")
