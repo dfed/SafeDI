02aa74d49b6ae3a9cd4b5cf4af10d63996b3bb66
Dan Federman <dfed@me.com>
2024-01-16 10:55:31 -0800

    Do not inspect static properties

--- Sources/SafeDICore/Visitors/InstantiableVisitor.swift
+++ Sources/SafeDICore/Visitors/InstantiableVisitor.swift
@@ -33,7 +33,10 @@ public final class InstantiableVisitor: SyntaxVisitor {
     // MARK: SyntaxVisitor
 
     public override func visit(_ node: VariableDeclSyntax) -> SyntaxVisitorContinueKind {
-        guard declarationType.isTypeDefinition else {
+        guard
+            declarationType.isTypeDefinition
+                && node.modifiers.staticModifier == nil
+        else {
             return .skipChildren
         }
         // Check attributes and extract dependency source.
