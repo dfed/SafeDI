5ea7e303c15617e9329dd528bec13bc1b67f858e
Dan Federman <dfed@me.com>
2024-02-01 09:11:58 -0700

    Improve error messages when missing required initializer (#54)
    Error message now explains what you need, and the FixIt explains why you needed it. Separation of concerns makes the experience more clear.

--- Sources/SafeDIMacros/Macros/InstantiableMacro.swift
+++ Sources/SafeDIMacros/Macros/InstantiableMacro.swift
@@ -84,9 +84,10 @@ public struct InstantiableMacro: MemberMacro {
                         ),
                         at: membersWithInitializer.startIndex
                     )
+                    // TODO: Create separate fixit if just `public` or `open` are missing.
                     context.diagnose(Diagnostic(
                         node: Syntax(declaration.memberBlock),
-                        error: FixableInstantiableError.missingRequiredInitializer,
+                        error: FixableInstantiableError.missingRequiredInitializer(hasInjectableProperties: !visitor.dependencies.isEmpty),
                         changes: [
                             .replace(
                                 oldNode: Syntax(declaration.memberBlock.members),
