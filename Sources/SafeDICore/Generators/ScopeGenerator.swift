5df901e28c37602dbf3e23a4422139f2eba374c6
Dan Federman <dfed@me.com>
2024-07-07 13:55:06 -0700

    Simplify

--- Sources/SafeDICore/Generators/ScopeGenerator.swift
+++ Sources/SafeDICore/Generators/ScopeGenerator.swift
@@ -351,47 +351,43 @@ actor ScopeGenerator: CustomStringConvertible, Sendable {
     private var generateCodeTask: Task<String, Error>?
 
     private var orderedPropertiesToGenerate: [ScopeGenerator] {
-        get throws {
-            var orderedPropertiesToGenerate = [ScopeGenerator]()
-            var propertyToUnfulfilledScopeMap = propertiesToGenerate
-                .reduce(into: OrderedDictionary<Property, ScopeGenerator>()) { partialResult, scope in
-                    if let property = scope.property {
-                        partialResult[property] = scope
-                    }
+        var orderedPropertiesToGenerate = [ScopeGenerator]()
+        var propertyToUnfulfilledScopeMap = propertiesToGenerate
+            .reduce(into: OrderedDictionary<Property, ScopeGenerator>()) { partialResult, scope in
+                if let property = scope.property {
+                    partialResult[property] = scope
                 }
-            func fulfill(_ scope: ScopeGenerator, stack: OrderedSet<Property> = []) throws {
-                guard
-                    let property = scope.property,
-                    propertyToUnfulfilledScopeMap[property] != nil
-                else {
-                    return
-                }
-                let scopeDependencies = propertyToUnfulfilledScopeMap
-                    .keys
-                    .intersection(scope.requiredReceivedProperties)
-                    .compactMap { propertyToUnfulfilledScopeMap[$0] }
-                // Fulfill the scopes we depend upon.
-                for dependentScope in scopeDependencies {
-                    var stack = stack
-                    stack.append(property)
-                    try fulfill(dependentScope, stack: stack)
-                }
-                // We can now be marked as fulfilled!
-                orderedPropertiesToGenerate.append(scope)
-                propertyToUnfulfilledScopeMap[property] = nil
             }
-
-            for scope in propertiesToGenerate {
-                try fulfill(scope)
+        func fulfill(_ scope: ScopeGenerator) {
+            guard
+                let property = scope.property,
+                propertyToUnfulfilledScopeMap[property] != nil
+            else {
+                return
             }
-
-            return orderedPropertiesToGenerate
+            let scopeDependencies = propertyToUnfulfilledScopeMap
+                .keys
+                .intersection(scope.requiredReceivedProperties)
+                .compactMap { propertyToUnfulfilledScopeMap[$0] }
+            // Fulfill the scopes we depend upon.
+            for dependentScope in scopeDependencies {
+                fulfill(dependentScope)
+            }
+            // We can now be marked as fulfilled!
+            orderedPropertiesToGenerate.append(scope)
+            propertyToUnfulfilledScopeMap[property] = nil
         }
+
+        for scope in propertiesToGenerate {
+            fulfill(scope)
+        }
+
+        return orderedPropertiesToGenerate
     }
 
     private func generateProperties(leadingMemberWhitespace: String) async throws -> [String] {
         var generatedProperties = [String]()
-        for childGenerator in try orderedPropertiesToGenerate {
+        for childGenerator in orderedPropertiesToGenerate {
             try await generatedProperties.append(
                 childGenerator
                     .generateCode(leadingWhitespace: leadingMemberWhitespace)
