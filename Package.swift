c9cd212bc473dc84308c13b720da71c0bb6e7164
Dan Federman <dan@portola.ai>
2025-05-29 12:52:00 -0700

    more explicit dependencies

--- Package.swift
+++ Package.swift
@@ -65,6 +65,9 @@ let package = Package(
 			dependencies: [
 				"SafeDICore",
 				.product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+				.product(name: "SwiftDiagnostics", package: "swift-syntax"),
+				.product(name: "SwiftSyntax", package: "swift-syntax"),
+				.product(name: "SwiftSyntaxBuilder", package: "swift-syntax"),
 				.product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
 			],
 			swiftSettings: [
