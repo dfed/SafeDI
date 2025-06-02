5bb54e05e3ca0c0d815f700c8580119e68e8cb78
Dan Federman <dfed@me.com>
2025-06-01 21:52:38 -0700

    Example adopts latest release candidate

--- Examples/ExamplePrebuiltPackageIntegration/Package.swift
+++ Examples/ExamplePrebuiltPackageIntegration/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
 		),
 	],
 	dependencies: [
-		.package(url: "https://github.com/dfed/SafeDI.git", exact: "1.2.0-rc-5"),
+		.package(url: "https://github.com/dfed/SafeDI.git", exact: "1.2.0-rc-6"),
 	],
 	targets: [
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
