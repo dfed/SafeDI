6a73174c5c37079ccedf62de4d6ba896af9f2a15
Dan Federman <dan@portola.ai>
2025-07-08 20:13:27 -0700

    Bump dependency in ExamplePrebuiltPackageIntegration

--- Examples/ExamplePrebuiltPackageIntegration/Package.swift
+++ Examples/ExamplePrebuiltPackageIntegration/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
 		),
 	],
 	dependencies: [
-		.package(url: "https://github.com/dfed/SafeDI.git", from: "1.2.0"),
+		.package(url: "https://github.com/dfed/SafeDI.git", from: "1.3.0"),
 	],
 	targets: [
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
